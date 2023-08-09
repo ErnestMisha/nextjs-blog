1fcd48e560c0e663873d339d67b9f19f7fc97231
Ernest Misha <pmiel13@gmail.com>
2023-08-09 13:23:44 +0200

    fix page title

--- pages/sample.jsx
+++ pages/sample.jsx
@@ -1,8 +1,12 @@
 import Layout from "../components/layout";
+import Head from "next/head";
 
 export default function Sample() {
   return (
     <Layout>
+      <Head>
+        <title>Hi!</title>
+      </Head>
       <h1>Hi!</h1>
     </Layout>
   );
